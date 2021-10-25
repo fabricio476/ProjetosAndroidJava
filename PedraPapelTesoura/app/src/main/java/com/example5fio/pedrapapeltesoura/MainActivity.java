d39c26d612fd75c4bf2cc36870bb0fe7f902f4cb
Fabrício R. de França <fabriciobtcc@gmail.com>
2021-10-25 03:46:05 -0300

    finalisado parte 1 do app pedra papel tesoura

--- PedraPapelTesoura/app/src/main/java/com/example5fio/pedrapapeltesoura/MainActivity.java
+++ PedraPapelTesoura/app/src/main/java/com/example5fio/pedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example5fio.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void sPedra(View view){
+       this.opcaoSelecionada("Pedra");
+
+    }
+    public void sPapel(View view){
+       this.opcaoSelecionada("Papel");
+    }
+    public void sTesoura(View view){
+        this.opcaoSelecionada("Tesoura");
+    }
+
+    
+    public void opcaoSelecionada(String opcaoSelecionada){
+
+
+    }
+
+
 }
