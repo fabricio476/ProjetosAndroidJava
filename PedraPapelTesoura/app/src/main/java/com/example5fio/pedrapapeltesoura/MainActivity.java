cb47524ec7c6e78d84ba77a75be4a77727a48693
Fabrício R. de França <fabriciobtcc@gmail.com>
2021-10-25 04:09:34 -0300

    adicionado a parte 2 do app

--- PedraPapelTesoura/app/src/main/java/com/example5fio/pedrapapeltesoura/MainActivity.java
+++ PedraPapelTesoura/app/src/main/java/com/example5fio/pedrapapeltesoura/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //metodos do onclik dos componentes da interface
     public void sPedra(View view){
        this.opcaoSelecionada("Pedra");
 
@@ -26,8 +30,37 @@ public class MainActivity extends AppCompatActivity {
         this.opcaoSelecionada("Tesoura");
     }
 
-    
+
     public void opcaoSelecionada(String opcaoSelecionada){
+
+        //pegar o coponente da interface
+        ImageView imageResultado = findViewById(R.id.id_image_resultado);
+
+        //gerar numeros aleatorios
+      int numero = new Random().nextInt(3);
+
+       String[] opcoes = {"pedra","papel","tesoura"}; //criar as opcoes
+       String opcaoApp = opcoes[numero]; //escolher umas das opcoes criadas no array
+
+        switch (opcaoApp){
+
+            case "pedra":
+
+                imageResultado.setImageResource(R.drawable.pedra); // metodo para trocar de imagem
+                break;
+
+            case "papel":
+
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+
+
+        }
+
 
 
     }
