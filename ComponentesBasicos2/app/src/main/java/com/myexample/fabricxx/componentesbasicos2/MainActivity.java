8f4c6720d1f948d4d7b60cbee1d03a981a0140a7
Fabrício R. de França <fabriciobtcc@gmail.com>
2021-10-29 02:44:15 -0300

    adicionado componentes Radiogroup e organizado medos para cada tipo de componente , e criado metodo de escutar botoes do radiogroup

--- ComponentesBasicos2/app/src/main/java/com/myexample/fabricxx/componentesbasicos2/MainActivity.java
+++ ComponentesBasicos2/app/src/main/java/com/myexample/fabricxx/componentesbasicos2/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     private CheckBox checkVerde, checkBranco, checkVermelho;
 
+    private RadioButton sexoMasculino, sexoFeminino;
+    private RadioGroup opcaoSexo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +32,95 @@ public class MainActivity extends AppCompatActivity {
         campoEmail = findViewById(R.id.id_CampEmail);
         textoResultado = findViewById(R.id.Id_TextResultado);
 
+        //checkBox
         checkVerde = findViewById(R.id.id_checkVerde);
         checkBranco = findViewById(R.id.id_checkBranco);
         checkVermelho = findViewById(R.id.id_checkVermelho);
 
+        //RadioButton
+        sexoMasculino = findViewById(R.id.id_RadioMasculino);
+        sexoFeminino = findViewById(R.id.id_RadioFeminino);
+        opcaoSexo = findViewById(R.id.id_RadioGroup);
+
+        RadioButton(); // metodo para ficar escutando o botão radio sempre que for alterado
 
     }
 
-    public void checkbox(){
+    public void RadioButton(){
 
+        //METODO DO RADIOGROUP PARA ESCUTAR A OPÇÃO SELECIONADA
+       opcaoSexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+               if(i == R.id.id_RadioMasculino){
+
+                  textoResultado.setText("Masculino");
+
+               }else if(i == R.id.id_RadioFeminino){
+
+                   textoResultado.setText("Feminino");
+               }
+           }
+       });
+
+
+
+
+
+      //   sexoMasculino.isChecked(); //retorna verdadeira ou falso de o RaioButons estiver selecionado
+
+
+        /*
+        //metodo para executar a opção selecionada
+        if(sexoMasculino.isChecked()){
+            textoResultado.setText("Masculino!");
+        }else if(sexoFeminino.isChecked()){
+            textoResultado.setText("Feminino!");
+        }
+
+         */
+
+
+    }
+
+
+
+
+
+    //metodo do botão SALVAR
+    public void enviar(View view){
+
+      //  checkbox(); //chama o metodo com as funcionalidades do checkbox
+
+      //  preencherTexto(); // chama o metodo de preencher os campos nome e email
+
+         RadioButton();
+
+    }
+
+    //metodo para o botão LIMPAR
+   public void limpar(View view){
+       campoNome.setText("");
+       campoEmail.setText("");
+       textoResultado.setText("");
+    }
+
+
+    //metodo para preencher os dados de email e nome
+    public void preencherTexto(){
+
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        textoResultado.setText("nome: " + nome + " email:" + email );
+
+    }
+
+
+
+
+    public void checkbox(){
         //boolean resultadoVerde = checkVerde.isChecked(); //retorna verdadeiro ou falso
         //textoResultado.setText("verde: " + resultadoVerde );
 
@@ -48,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
             texto = corSelecionada;
 
 
-         //   texto = "verde selecionado - ";
+            //   texto = "verde selecionado - ";
         }
 
         if( checkBranco.isChecked() ){
@@ -60,26 +145,7 @@ public class MainActivity extends AppCompatActivity {
             texto = texto + "Vermelho selecionado - ";
         }
 
-      textoResultado.setText(texto);
-
-    }
-
-
-    public void enviar(View view){
-
-        checkbox();
-
-       // String nome = campoNome.getText().toString();
-       // String email = campoEmail.getText().toString();
-
-      //  textoResultado.setText("nome: " + nome + " email:" + email );
-    }
-
-   public void limpar(View view){
-
-       campoNome.setText("");
-       campoEmail.setText("");
-       textoResultado.setText("");
+        textoResultado.setText(texto);
 
     }
 
