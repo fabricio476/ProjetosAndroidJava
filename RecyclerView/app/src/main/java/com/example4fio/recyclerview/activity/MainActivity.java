e36f54d21bc1f0ad7fac13bfb7cf4ae1d6b7d3a6
Fabrício Rosa de França <fabricio476@outlook.com.br>
2022-09-13 00:02:28 -0300

    concluido a recicler view part2

--- RecyclerView/app/src/main/java/com/example4fio/recyclerview/activity/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example4fio/recyclerview/activity/MainActivity.java
@@ -2,14 +2,22 @@ package com.example4fio.recyclerview.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 import com.example4fio.recyclerview.R;
+import com.example4fio.recyclerview.adapter.Adapter;
+import com.example4fio.recyclerview.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
      private RecyclerView recyclerView;
+     private List<Filme> listaFilmes = new ArrayList<Filme>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView); //instanciar o recicler view
+        this.criarFilmes();
+
+        //intenciado o adpter que foi criado
+        Adapter adpter = new Adapter(listaFilmes);
 
         //criar e definir o layout do recyclerview
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -26,11 +38,33 @@ public class MainActivity extends AppCompatActivity {
          //melhorar a performace
          recyclerView.setHasFixedSize(true); //metodo informa que vai posuir um tamanho fixo
 
+        //Para criar uma linha divisoria entre os itens
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
 
-
-
+         recyclerView.setAdapter(adpter); //adicionado na recycler view o adpter que foi criado
 
     }
 
+    public void criarFilmes() {
+
+        Filme filme = new Filme("it: A Coisa", "Terror", "2017");
+        this.listaFilmes.add(filme);
+        filme = new Filme("A mumia", "terror", "2017");
+        this.listaFilmes.add(filme);
+        filme = new Filme("A bela e a fera", "romance", "2018");
+        this.listaFilmes.add(filme);
+        filme = new Filme("carros 3", "comédia", "2000");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Homem-Aranha", "aventura", "1999");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Mulher maravilha", "fantasia", "2002");
+        this.listaFilmes.add(filme);
+        filme = new Filme("pantera negra", "ação", "2015");
+        this.listaFilmes.add(filme);
+        filme = new Filme("quarteto fantastico", "acão", "2014");
+        this.listaFilmes.add(filme);
+        filme = new Filme("poderoso chefão", "drama", "2005");
+        this.listaFilmes.add(filme);
+    }
 
 }
--- RecyclerView/app/src/main/java/com/example4fio/recyclerview/adapter/Adapter.java
+++ RecyclerView/app/src/main/java/com/example4fio/recyclerview/adapter/Adapter.java
@@ -2,30 +2,53 @@ package com.example4fio.recyclerview.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example4fio.recyclerview.R;
+import com.example4fio.recyclerview.model.Filme;
+
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
+  private List<Filme> listaFilmes;
+
+    public Adapter(List<Filme> lista) {
+        this.listaFilmes = lista;
+    }
 
     @NonNull
     @Override //CRIAR AS VISULIZAÇÕES
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+
+        //criado uma objeto do tipo View para esibir na lista
+        View itemLista = LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.view_adpter_lista,viewGroup,false);
+
+
+        return new MyViewHolder(itemLista); //constroe a view que vai aparecer ma tela
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
 
+        Filme filme = listaFilmes.get(i);
+        myViewHolder.titulo.setText(filme.getTituloFilme());
+        myViewHolder.genero.setText(filme.getGenero());
+        myViewHolder.ano.setText(filme.getAno());
     }
 
-    @Override
+    @Override// quantidade de items que vão ser exibidos por vez
     public int getItemCount() {
-        return 0;
+
+        return listaFilmes.size();
     }
 
-    //uma innerclass - essa classe esta dentro de outra classe
+    //uma innerclass - essa classe esta dentro de outra classe (MyViewHolder - é o objeto modelo com as informações)
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
         TextView titulo;
@@ -34,8 +57,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
 
         public MyViewHolder(View itemView) {
-
             super(itemView);
+
+            //conteudo que vai ser mostrado na tela
+           titulo = itemView.findViewById(R.id.id_titulo);
+           ano = itemView.findViewById(R.id.id_ano);
+           genero = itemView.findViewById(R.id.id_genero);
+
+
         }
     }
 
